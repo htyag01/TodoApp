8116355e058457f06813aeaba01b26416d916c1f
Harshit Tyagi <htyagi01@gmail.com>
2022-05-17 23:52:03 +0530

    Update Home.js

--- src/component/Home.js
+++ src/component/Home.js
@@ -8,7 +8,7 @@ function Home () {
             <div>
                 <div>
                     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                        <Link class="navbar-brand" to="/Home">Todo App Demo 2</Link>
+                        <Link class="navbar-brand" to="/Home">Todo App Demo 3</Link>
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
